efa973fb1950dd17f46dbe20cf902c6a98c6735f
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2023-05-19 09:18:38 +0300

    work on home page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,30 @@
 import { getProjects } from "@/sanity/sanity-utils";
+import { Project } from "@/types/project";
+import Image from "next/image";
 
 export default async function Home() {
   const projects = await getProjects();
   return (
-    <div>
-      {projects.map((project: any) => (
-        <div key={project._id}>{project.name}</div>
-      ))}
+    <div className="max-w-5xl mx-auto py-20">
+      <h1 className="text-7xl font-extrabold">hello</h1>
+      <p className="mt-3 text-xl text-gray-600">check out my projects</p>
+      <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Projects</h2>
+      <div className="mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-8 p-3" >
+        {projects.map((project) => (
+          <div key={project._id} className="border border-gray-500 rounded-lg">
+            {project.image && (
+              <Image
+                src={project.image}
+                alt={project.name}
+                width={250}
+                height={100}
+                className="object-cover rounded-lg border-gray-500"
+              />
+            )}
+            <div className="font-extrabold">{project.name}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- client/sanity/sanity-utils.ts
+++ client/sanity/sanity-utils.ts
@@ -1,6 +1,7 @@
+import { Project } from "@/types/project";
 import { createClient, groq } from "next-sanity";
 
-export const getProjects = async () => {
+export const getProjects = async (): Promise<Project[]> => {
   const client = createClient({
     projectId: "4e7mvi1j",
     dataset: "production",
